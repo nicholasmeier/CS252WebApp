5a11fca7f90c48bcafa1f5e732ea57ee2df067ca
nicholasmeier <nmeier@purdue.edu>
2018-04-15 18:09:16 -0400

    Added some js files, still messing around

---
+++ test.js
@@ -0,0 +1,18 @@
+window.onload=function() {
+	canv=document.getElementById("test");
+	ctx=canv.getContext("2d");
+	document.addEventListener("keydown", keyPush);
+}
+
+function keyPush(evt){
+	switch(evt.keyCode){
+		case 37:
+			break;
+		case 38:
+			break;
+		case 39:
+			break;
+		case 40:
+			break;
+	}
+}
