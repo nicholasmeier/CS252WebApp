0dd1d0321e57caf762e598622710bd5ce33337ca
nicholasmeier <nmeier@purdue.edu>
2018-04-16 20:20:59 -0400

    made a movable character in js, added assets directory and stuff with it

--- test.js
+++ test.js
@@ -3,37 +3,53 @@ var ctx = document.getElementById("ctx").getContext("2d");
 ctx.font = '30px Arial';
 ctx.fillStyle = 'green';
 
-var x = 100;
-var y = 100;
+var x = 50;
+var y = 50;
+var player = new Image;
+ctx.drawImage(player, x, y);
 
-ctx.fillText('A',y,x);
+//ctx.fillText('A',y,x);
 
 document.addEventListener("keydown", keyPush);
-/*
-window.onload=function() {
-	canv=document.getElementById("test");
-	ctx=canv.getContext("2d");
-	document.addEventListener("keydown", keyPush);
-}*/
+
+setInterval(update, 5);
+
+
+function update(){
+	ctx.fillStyle = 'white';
+	ctx.fillRect(0,0,500,500);
+	ctx.drawImage(player, x, y);
+	/*ctx.fillStyle = 'green';
+	ctx.fillText('A',x,y);*/
+}
 
 function keyPush(evt){
 	switch(evt.keyCode){
 		case 37:
 			//ctx.clearRect(0,0,canvas.width,canvas.height);
-			x-=25;
-			ctx.fillText('A',x,y);
+			if (x > 0){
+				x-=25;
+				ctx.fillText('A',x,y);	
+			}
 			break;
 		case 38:
-			y-=25;
-			ctx.fillText('A',x,y);
+			if (y > 25){
+				y-=25;
+				ctx.fillText('A',x,y);	
+			}
 			break;
 		case 39:
-			x+=25;
-			ctx.fillText('A',x,y);
+			if (x < 475){
+				x+=25;
+				ctx.fillText('A',x,y);	
+			}
 			break;
 		case 40:
-			y+=25;
-			ctx.fillText('A',x,y);
+			if (y < 500){
+				y+=25;
+				ctx.fillText('A',x,y);	
+			}
 			break;
 	}
 }
+player.src = 'assets/player.png';
